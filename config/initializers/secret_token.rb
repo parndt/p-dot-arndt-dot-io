d9dd5d8fac0df21717f5b1442433c64e2eb520fc
Philip Arndt <parndt@gmail.com>
2013-02-26 10:00:30 +1300

    Remove secret_token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PhilipArndt::Application.config.secret_token = 'dac871ea7718a6d520416cc2c76e6f87b72b26462c13d1a943d79f35059d9c067158e859898021902cf36acc9700fd08a50540a8e497c603d245ec8d51f26a68'
+PhilipArndt::Application.config.secret_token = ENV['SECRET_TOKEN'] || 5.times.map {|s| SecureRandom.hex}.join('')
