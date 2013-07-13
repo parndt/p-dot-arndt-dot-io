3777753cc0d03371b8959b99b660e45de28ac55c
Philip Arndt <p@arndt.io>
2013-07-14 04:57:36 +1200

    Updated the layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-
-  def hearts
-    colours = ['blue', '', 'purple', 'green', 'yellow', 'kissing', 'gift'].map{|c|
-      c.present? ? "#{c}_" : c
-    }
-    raw 7.times.inject('') { |images|
-      images << image_tag("http://www.emoji-cheat-sheet.com/graphics/emojis/#{colours.sample}heart.png", :size => '14x14', :alt => '<3')
-    }
-  end
-
 end
