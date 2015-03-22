8e65ca7dc2eff3eb65b4f1b02e69b887ce46eeaa
Philip Arndt <git@p.arndt.io>
2015-03-23 11:47:13 +1300

    Changed email domain away from Heroku

--- config/initializers/heroku.rb
+++ config/initializers/heroku.rb
@@ -6,6 +6,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
+  :domain         => 'p.arndt.io'
 }
 ActionMailer::Base.delivery_method = :smtp
